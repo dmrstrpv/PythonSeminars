6865038515343a4dad24f98b09071d7983b95620
dmrstrpv <dmrstrpv@gmail.com>
2022-12-23 18:58:57 +0300

    homework

---
+++ seminar4_hw/task3.py
@@ -0,0 +1,10 @@
+# Задайте последовательность чисел. Напишите программу,
+# которая выведет список неповторяющихся элементов исходной последовательности.
+
+import random
+
+list = [random.randint(0,10) for i in range (10)]
+print(list)
+list1 = []
+[list1.append(i) for i in list if i not in list1]
+print(list1)
